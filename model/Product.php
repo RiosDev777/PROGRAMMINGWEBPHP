d7180ecc4a8814c185bdf0bc1f08f6ef7647b669
JUAN J RIOS <juangaming070@gmail.com>
2025-03-08 07:41:03 -0500

    Class Product

--- model/Product.php
+++ model/Product.php
@@ -1,2 +1,15 @@
 <?php 
+class Product {
+    var $code;
+    var $name;
+    int $stock;
+    double $valorUnitario;
+
+    function __construct($code, $name, $stock, $valorUnitario) {
+        $this->code = $code;
+        $this->name = $name;
+        $this->stock = $stock;
+        $this->valorUnitario = $valorUnitario;
+    }
+}
 ?>
